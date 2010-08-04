307d8ff24a581633dd894c7f4666d9dab1cec09e
Ben Zittlau <ben@zittlau.ca>
2010-08-03 18:03:08 -0600

    Page controller and blueprint css added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{h(@title)}"
+    end
+  end
+  
+  #Returns an image tag containing the project logo
+  def logo
+    image_tag "logo.png", :alt => "Sample App", :class => "round"
+  end
 end
