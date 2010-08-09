7b042c39feae2614aea18afccd82cd9cf763cf6e
Ben Zittlau <ben@zittlau.ca>
2010-08-09 00:47:29 -0600

    Refactoring the garbage subscription so it's much cleaner now

--- app/models/garbage_subscription.rb
+++ app/models/garbage_subscription.rb
@@ -6,25 +6,26 @@ class GarbageSubscription < ServiceSubscription
   
   #method to determine whether an alert should be sent to the subscribed user
   def alert_user?
-    send_an_alert = false
-    #first check whether the time approximately matches the delivery time
-    if self.enabled? && self.approximately_now?
-        # check if there is a garbage pickup today
-        # this will need to get more intelligent to make sure we are on the right
-        # side of the garbage pickup
-        now = DateTime.now
-        #define the date range to check
-        today = now.to_date
-        tomorrow = 1.day.since(now).to_date
-      
-        #unless there are no pickups today, send the user an alert 
-        unless GarbagePickup.find(:all, 
-                            :conditions => {:pickup_date => today..tomorrow, 
-                                            :zone =>        self.zone,
-                                            :day =>       self.day}).empty?
-          send_an_alert = true
-        end
-      end
-    return send_an_alert
+    # check the three necessary conditions
+    # in order of increasing cost
+    self.enabled? && self.approximately_now? && pickup_today?
   end
+  
+  private 
+    def pickup_today?
+      # this will need to get more intelligent in order to better handle
+      # timezones so that the notice always comes before the pickup date
+      # in particular look at the case of a notification at midnight
+      now = DateTime.now
+      #define the date range to check
+      today = now.to_date
+      tomorrow = 1.day.since(now).to_date
+  
+      #return true if there is a pickup today, false if it's empty
+      GarbagePickup.find(:all, 
+                          :conditions => {:pickup_date => today..tomorrow, 
+                                          :zone =>        self.zone,
+                                          :day =>       self.day}).present?
+    end
+  
 end
