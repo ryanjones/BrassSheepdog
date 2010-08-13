733568efd6e1a47d6e8d2e8d892dd361056d2810
Ben Zittlau <ben@zittlau.ca>
2010-08-12 23:16:29 -0600

    Commiting the addition of the zone dropdown in the garbage zone settings

--- app/models/garbage_subscription.rb
+++ app/models/garbage_subscription.rb
@@ -1,11 +1,35 @@
 class GarbageSubscription < ServiceSubscription
-  
-  attr_accessible :zone, :day
+  attr_accessible :formatted_zone, :delivery_time, :day_before
   
   validates_presence_of :zone, :on => :update
   validates_presence_of :day, :on => :update
   validates_presence_of :delivery_time, :on => :update
   validates_presence_of :day_before, :on => :update
+  
+  #hard-coding the valid zones for now, this might need to change
+  #if we want to support more cities, but auto-importing from 
+  #the data set
+  VALID_ZONES = ['A1', 'A2', 'B2', 'B3', 'B4', 'C4', 'C5', 'D5', 'D6', 'D7', 'E7', 'E8'].freeze
+
+  #define a class method to check the validity of a zone
+  def self.valid_zone?(zone)
+    GarbageSubscription::VALID_ZONES.include?(zone)
+  end
+  
+  #define a class method to check the validity of a zone
+  def self.valid_zones
+    GarbageSubscription::VALID_ZONES
+  end  
+  
+  #set up a pseudo property for the formatted zone
+  def formatted_zone 
+    self.zone + self.day.to_s
+  end
+  
+  def formatted_zone=(new_zone)
+    self.zone = new_zone[0].chr
+    self.day = new_zone[1].chr
+  end
   
   #method to determine whether an alert should be sent to the subscribed user
   def alert_user?
