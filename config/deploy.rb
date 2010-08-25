2bb568a947fb4fcdd63f23a23d7b365470f5c0c2
Ben Zittlau <ben@zittlau.ca>
2010-08-25 11:05:04 -0600

    Updating the favicon to greg's

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,12 @@ namespace :deploy do
     top.deprec.app.restart
   end
 end
+
+after "deploy:symlink", "deploy:update_crontab"
+
+namespace :deploy do
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && whenever --update-crontab #{application}"
+  end
+end
