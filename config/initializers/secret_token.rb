a083e0eb5a3ec5fa3a64d0665c8f6d9284898e96
Ben Zittlau <ben@zittlau.ca>
2011-04-17 14:13:46 -0600

    Forgot to git add first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrassSheepdog::Application.config.secret_token = '5bc0cb023a1c41270688dde82fe9ddeeff609b63ec8d3eb6533981287bbefad0e77129ba3c8f3fa0bcfdf5111165921c1ac60b1ef6726b4cd8ff0f8c57e583ce'
