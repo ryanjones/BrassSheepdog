4fea6bfcbb08a3af557b165a5d85bf05b801fcfb
Ben Zittlau <ben@zittlau.ca>
2011-06-20 19:00:55 -0600

    After installing rails 3 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alertzy::Application.config.secret_token = '4292ecd9dec36b7e25ab5679f70dcafab468938e66bc24ac57c39dc048c4363d6613bdb6408cb520c14e0610f3499c829308c09ad7fff4c8f7e2ca989c07c253'
