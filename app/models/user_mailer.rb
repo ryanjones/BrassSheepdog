5942d3bc2deb02cdccc5a2586ae2f22221a8633b
Ben Zittlau <ben@zittlau.ca>
2011-07-21 20:55:18 -0600

    Convering the rest of the mailers over the the rails 3 mailer format.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,36 +1,22 @@
 class UserMailer < ActionMailer::Base
-  def registration_notification(user)
-    setup_email(user)
-    subject    "Thank you for registering for Alertzy!"
-    content_type "multipart/alternative"
+  default :from => "Alertzy.com Update <update@alertzy.com>", :subject => ""
 
-    part "text/plain" do |p|
-      p.body = render_message("registration_notification_text", :user => user)
-      p.transfer_encoding = "base64"
-    end
+  def registration_notification(user)
+    subject = "Thank you for registering for Alertzy!"
     
-    part :content_type => "text/html",
-      :body => render_message("registration_notification_html", :user => user)
+    @user = user
+
+    mail(:to => "#{user.email}",
+         :subject => subject)
+
   end
   
   def reminder_email(user)
-    setup_email(user)
-    subject "Alertzy Forgotten Login/Password"
-    content_type "multipart/alternative"
-
-    part "text/plain" do |p|
-      p.body = render_message("reminder_email_text", :user => user)
-      p.transfer_encoding = "base64"
-    end
+    subject = "Thank you for registering for Alertzy!"
     
-    part :content_type => "text/html",
-      :body => render_message("reminder_email_html", :user => user)
-  end
-  
-  def setup_email(user)
-    recipients   "#{user.email}"
-    from         "Alertzy.com Update" "<updates@alertzy.com>"
-    subject      ""
-    sent_on      Time.now
+    @user = user
+
+    mail(:to => "#{user.email}",
+         :subject => subject)
   end
 end
