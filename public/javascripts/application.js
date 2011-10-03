72264a195439d12ce2e369a9bcbcde9992a9a881
Ben Zittlau <ben@zittlau.ca>
2011-10-02 21:10:31 -0600

    Updating more copy, making a learn more option for new services open get satisfaction, and fixing a few additional css bugs.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function($) {
-    $("#service_subscription_address").change(function() {
-        $.ajax({url: '/addresses/lookup_zone',
-        data: 'address=' + this.value,
-        dataType: 'script'})
-        $('#loading-image').show();
-    });
+  $(".feedback_widget_opener").click(function(event) {
+    $("#fdbk_tab").click();
+    event.preventDefault();
+  })
+  $("#service_subscription_address").change(function() {
+      $.ajax({url: '/addresses/lookup_zone',
+      data: 'address=' + this.value,
+      dataType: 'script'})
+      $('#loading-image').show();
+  });
 });
 // 
 //
