6a2f6f014c0ef78fef2f18098d24c1f1a18a96fb
Ryan Jones <ryan.michael.jones@gmail.com>
2011-10-22 22:56:45 -0600

    Converted rails 2 -> rails 3 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,69 +1,87 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :service_subscriptions
-
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.login  '/login',  :controller => 'sessions', :action => 'new'
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.explicit_edit '/edit', :controller => 'users', :action => 'explicit_edit'
   
-  map.unsubscribe '/unsubscribe', :controller => 'service_subscriptions', :action => 'destroy'
-
-  map.resources :users, :collection => { :forgot => :get, :remind => :post}, :member => { :reset => :put, :new_password => :get}
-  map.resource :sms_message, :only => [:new, :create, :incoming], :member => {:incoming => :post}
-  map.resource :address, :only => [:new, :create]
-  map.resource :session
-  map.resources :services, :only => [:index]
-
-  map.contact '/contact', :controller => 'pages', :action => 'contact'
-  map.services_list   '/services_list',   :controller => 'pages', :action => 'services'
-  map.about   '/about',   :controller => 'pages', :action => 'about'
-  map.faq    '/faq',    :controller => 'pages', :action => 'faq'
+  # Singular Resources
+  match 'signup' => 'users#new'
+  match 'login' => 'sessions#new'
+  match 'logout' => 'sessions#destroy'
+  match 'edit' => 'users#explicit_edit'
+  match 'unsubscribe' => 'service_subscriptions#destroy'
   
-  map.verify  '/verify', :controller => 'users', :action => 'verify'
-  map.validation  '/validation', :controller => 'users', :action => 'validation_page'
-  map.resend_verification  '/resend_verification', :controller => 'users', :action => 'resend_verification'
+  match 'contact' => 'pages#contact'
+  match 'services_list' => 'pages#services'
+  match 'about' => 'pages#about'
+  match 'faq' => 'pages#faq'
+  
+  match 'verify' => 'users#verify'
+  match 'validation' => 'users#validation_page'
+  match 'resend_verification' => 'users#resend_verification'
+
+  # Resources
+  resources :users do
+      collection do 
+        get :forgot
+        post :remind
+      end
+      
+      member do
+        get :new_password
+        put :reset
+      end
+    end
+  
+  resources :service_subscriptions
+  resources :services, :only => [:index]
+  
+  # Resource
+  resource :sms_message, :only => [:new, :create, :incoming] do
+      member do
+        post :incoming
+      end
+    end
+  resource :address, :only => [:new, :create]
+  resource :session
+
+  # Root
+  root :to => 'pages#home'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  #   connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  #   purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  #   resources :products
 
   # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  #   resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
 
   # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+  #   resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
   
   # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
+  #   resources :products do |products|
   #     products.resources :comments
   #     products.resources :sales, :collection => { :recent => :get }
   #   end
 
   # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
+  #   namespace :admin do |admin|
   #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
   #     admin.resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-  map.root :controller => "pages", :action => "home"
-
-
-  # See how all your routes lay out with "rake routes"
+  # See how all your routes lay out with 'rake routes'
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  #connect ':controller/:action/:id'
+  #connect ':controller/:action/:id.:format'
 
 end
